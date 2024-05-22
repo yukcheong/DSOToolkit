2656cd5b765252023126a17142c6cd1863bae15f
yukcheong <tangyukcheong@live.com>
2024-05-22 17:34:13 +0100

    Update Robust Design.py

--- Robust Design.py
+++ Robust Design.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.integrate import quad
-from scipy.optimize import minimize
+from scipy.optimize import differential_evolution
 
 def f(x):
     return x + 1 / x
@@ -19,13 +19,13 @@ def mean_performance_index_to_minimize(x, noise):
 noise = 0.75
 lower_bound = 0.8
 upper_bound = 2
-initial_guess = [1.0]  # Initial guess for the optimizer
+#initial_guess = [1.0]  # Initial guess for the optimizer
 
-result_nominal_design = minimize(f, initial_guess, bounds=[(lower_bound, upper_bound)])
+result_nominal_design = differential_evolution(f, bounds=[(lower_bound, upper_bound)])
 optimal_x_nominal_design = result_nominal_design.x[0]
 penalty_nominal = np.abs(mean_performance_index(optimal_x_nominal_design, noise)-f(optimal_x_nominal_design))/f(optimal_x_nominal_design)
 
-result_mean_curve = minimize(mean_performance_index_to_minimize, initial_guess, args=(noise,), bounds=[(lower_bound, upper_bound)])
+result_mean_curve = differential_evolution(mean_performance_index_to_minimize, args=(noise,), bounds=[(lower_bound, upper_bound)])
 optimal_x_mean_curve = result_mean_curve.x[0]
 minimum_x_based_on_mean = f(optimal_x_mean_curve)
 penalty_mean = np.abs(mean_performance_index(optimal_x_mean_curve, noise)-f(optimal_x_nominal_design))/f(optimal_x_nominal_design)
